0c8817db1528cdb074fe7af964d80370d79322d6
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-12-15 15:03:45 +0530

    Initial commit

---
+++ MapkitInSwiftUI/MapkitInSwiftUI/MapkitInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapkitInSwiftUIApp.swift
+//  MapkitInSwiftUI
+//
+//  Created by Neosoft on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MapkitInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
